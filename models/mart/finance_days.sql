13ed809b3a704c0ace572d29d7dd325ba103a9a2
Théo WITTKE <theo.wittke@gmail.com>
2024-12-29 18:28:17 +0000

    fb

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -4,4 +4,15 @@ sales AS (
     SELECT * FROM {{ref("int_orders_operational")}}
 )
 
-SELECT * FROM sales
+SELECT
+    date,
+    FLOOR(COUNT(orders_id)) AS nb_transactions,
+    ROUND(SUM(revenue),2) AS revenue,
+    ROUND(AVG(revenue),2) AS avg_basket,
+    ROUND(SUM(operational_margin),2) AS operational_margin,
+    ROUND(SUM(purchase_cost),2) AS purchase_cost,
+    ROUND(SUM(shipping_fee),2) AS shipping_fee,
+    ROUND(SUM(logcost),2) AS log_cost,
+    FLOOR(SUM(quantity)) AS quantity
+FROM sales
+GROUP BY date ORDER BY date DESC
